c7e7906861c6b4adf7eba6dbd42ca12023d30277
Richard Gal <rgal75@gmail.com>
2025-03-14 19:08:40 +0100

    Initial Commit

---
+++ Game2048/Game2048App.swift
@@ -0,0 +1,17 @@
+//
+//  Game2048App.swift
+//  Game2048
+//
+//  Created by Richard Gal on 2025. 03. 14..
+//
+
+import SwiftUI
+
+@main
+struct Game2048App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Game2048Tests/Game2048Tests.swift
@@ -0,0 +1,16 @@
+//
+//  Game2048Tests.swift
+//  Game2048Tests
+//
+//  Created by Richard Gal on 2025. 03. 14..
+//
+
+import Testing
+
+struct Game2048Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
